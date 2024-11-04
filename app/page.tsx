57a1af39228bf1907dae3ac1814c7d2d3e4409e2
NghiepDev <me@nghiep.dev>
2024-11-05 01:26:11 +0700

    Add twitter capture

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,27 @@
+import Image from 'next/image';
+import clsx from 'clsx';
+
 import Hero from './hero';
+import t1 from './t1.png';
+import t2 from './t2.png';
 
 export default function Home() {
   return (
     <div className='mx-auto min-h-screen max-w-screen-2xl'>
       <Hero />
+      <div className='bg-gra-1 py-10'>
+        <div
+          className={clsx(
+            'flex items-center justify-center',
+            '*:multi-[max-w-[min(550px,40%)]]',
+            'gap-10 lg:gap-20 xl:gap-32',
+            'max-md:multi-[`flex-col;[&>*]:max-w-[80%]`]'
+          )}
+        >
+          <Image src={t1} alt='' />
+          <Image src={t2} alt='' />
+        </div>
+      </div>
     </div>
   );
 }
