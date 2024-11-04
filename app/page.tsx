c396d231656e0d419fd5b14f743cbd73cc3203d0
NghiepDev <me@nghiep.dev>
2024-11-05 01:55:05 +0700

    Add video

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image';
 import clsx from 'clsx';
 
 import Hero from './hero';
+import VideoIntro from './video-intro';
+
 import t1 from './t1.png';
 import t2 from './t2.png';
 
@@ -15,11 +17,14 @@ export default function Home() {
             'flex items-center justify-center',
             '*:multi-[max-w-[min(550px,40%)]]',
             'gap-10 lg:gap-20 xl:gap-32',
-            'max-md:multi-[`flex-col;[&>*]:max-w-[80%]`]'
+            'max-md:multi-[`flex-col;[&>*]:max-w-[90%]`]'
           )}
         >
           <Image src={t1} alt='' />
           <Image src={t2} alt='' />
+        </div>
+        <div className='mx-5 mt-10 flex justify-center sm:mx-10 lg:mt-20'>
+          <VideoIntro />
         </div>
       </div>
     </div>
